18b22eadecf009ccdb0bcd673578e02fe7eb6404
Matthew Incardona <doublethink.inc@gmail.com>
2023-12-28 15:55:08 -0500

    Add insert note functionality

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { Ai } from '@cloudflare/ai'
 import { Hono } from 'hono'
 
+import write from './write.html'
 import ui from './ui.html'
 
 const app = new Hono()
@@ -25,10 +26,47 @@ app.get("/query", async c => {
 			]
 		}
 	)
+
 	return c.text(answer)
 })
 
 // 3. the ui for adding notes
+app.get("/write", c => {
+	return c.html(write)
+})
+
 // 4. a POST endpoint to add notes
+// 4a - insert note into d1 database
+app.post("/notes", async c => {
+	const ai = new Ai(c.env.AI)
+
+	const { text } = await c.req.json();
+	if (!text) c.throw(400, "Missing text");
+
+	const { results } = await c.env.DATABASE.prepare("INSERT INTO notes (text) VALUES (?) RETURNING *")
+		.bind(text)
+		.run()
+
+	const record = results.length ? results[0] : null
+
+	if (!record) c.throw(500, "Failed to create note")
+
+	// 4b - generate an embedding based on our note
+	const { data } = await ai.run('@cf/baai/bge-base-en-v1.5', { text: [text] })
+	const values = data[0]
+
+	if (!values) c.throw(500, "Failed to generate vector embedding")
+
+	// 4c - insert embedding into vectorize
+	const { id } = record
+	const inserted = await c.env.VECTOR_INDEX.upsert([
+		{
+			id: id.toString(),
+			values,
+		}
+	]);
+
+	return c.json({ id, text, inserted });
+})
 
 export default app
