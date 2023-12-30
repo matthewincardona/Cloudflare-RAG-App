2b35a8a3f7a698a9e997e21a52530f14b109fac5
Matthew Incardona <doublethink.inc@gmail.com>
2023-12-29 20:45:25 -0500

    Fix issues with chat stream

--- src/index.js
+++ src/index.js
@@ -13,21 +13,63 @@ app.get("/", c => {
 
 // 2. a POST endpoint to query the LLM
 app.get("/query", async c => {
-	const ai = new Ai(c.env.AI)
+	try {
+	const ai = new Ai(c.env.AI);
 
-	const question = c.req.query("text") || "What is the square root of 9?"
+	const question = c.req.query('text') || "What is the square root of 9?"
+
+	const embeddings = await ai.run('@cf/baai/bge-base-en-v1.5', { text: [ question ] })
+	const vectors = embeddings.data[0]
+  
+	const SIMILARITY_CUTOFF = 0.75;
+	const vectorQuery = await c.env.VECTOR_INDEX.query(vectors, { topK: 1 }) || { matches: [] }; // Default to an empty array if vectorQuery is falsy
+	const vecIds = vectorQuery.matches
+	  ? vectorQuery.matches
+		  .filter(vec => vec.score > SIMILARITY_CUTOFF)
+		  .map(vec => vec.vectorId)
+	  : [];
+	  
+	let notes = [];
+	if (vecIds.length) {
+	  const query = `SELECT * FROM notes WHERE id IN (${vecIds.join(", ")})`;
+	  const { results } = await c.env.DATABASE.prepare(query).bind().all();
+	  if (results) notes = results.map(vec => vec.text);
+	}
+	
+	const contextMessage = notes.length
+	  ? `Context:\n${notes.map(note => `- ${note}`).join("\n")}`
+	  : "";
+	
+  
+	const systemPrompt = `When answering the question or responding, use the context provided, if it is provided and relevant.`
+  
+	// const { response: answer } = await ai.run(
+	//   '@cf/meta/llama-2-7b-chat-int8',
+	//   {
+	// 	inputs: [
+	// 	  ...(notes.length ? [{ role: 'system', content: contextMessage }] : []),
+	// 	  { role: 'system', content: systemPrompt },
+	// 	  { role: 'user', content: question }
+	// 	]
+	//   }
+	// )
 
 	const { response: answer } = await ai.run(
 		"@cf/meta/llama-2-7b-chat-int8",
 		{
 			messages: [
 				{ role: "system", content: "You are a helpful assistant" },
-				{ role: "user", content: question },
+				{ role: "user", content: question }
 			]
 		}
 	)
+  
+	return c.text(answer);
 
-	return c.text(answer)
+	} catch (error) {
+        console.error("Error processing /query endpoint:", error);
+        return c.text("Internal Server Error", 500);
+    }
 })
 
 // 3. the ui for adding notes
